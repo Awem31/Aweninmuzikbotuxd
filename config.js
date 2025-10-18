33da0cbf5079f946dbec3fd350ca788b9efd7b35
Awem31 <cansneikoglu@gmail.com>
2025-10-18 12:52:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,23 +4,23 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Emirin Sözünü", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "discord.com/invite/8XSVzGFtqH",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
       name: "GlaceYT",
-      password: "glace",
-      host: "87.106.62.92",
-      port:  11642,
+      password: "Yuvraj.apk",
+      host: "noel.hidencloud.com",
+      port:  24642,
       secure: false
     }
   ]
